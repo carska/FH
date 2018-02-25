d692e67d3941fc37e302c158ae9565ff788a1b41
carska <35843275+carska@users.noreply.github.com>
2018-02-25 21:32:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <title></title>
+    </head>
+    <body>
+        <div class="containerHead">
+            <div class="logo">
+                <img src="img/Logo.png">
+            </div>
+            <ul class="nav">
+                <li><a href="">Kontakt</a></li>
+                <li><a href="">Personal</a></li>
+                <li><a class="active"href="">Galleri</a></li>
+                <li><a href="">Referenser</a></li>
+                <li><a href="">Om oss</a></li>
+                <li><a href="">Hem</a></li>
+            </ul>
+        </div>
+        <img class="backgroundImage" src="img/Toppbild.png">
+        <div class="container">
+            <div class="line">
+            </div>
+            <div class="column">
+                <p class="head">FAKTA</p>
+                <div class="row">
+                    <p class="rowPartOne">HUSTYP</p>
+                    <p class="rowPartTwo">1,5 planshus</p>
+                </div>  
+                <div class="row">
+                    <p class="rowPartOne">BOYTA</p>
+                    <p class="rowPartTwo">125 m²</p>
+                </div>
+                <div class="row">
+                    <p class="rowPartOne">ANTAL SOVRUM</p>
+                    <p class="rowPartTwo">4 sovrum</p>
+                </div>
+            </div>
+            <div class="column">
+                <p class="head">PLANRITNING</p>
+                <div class="row">
+                    <div class="rowPartOne">
+                        <img src="img/reader.png">
+                        <a href="">Planskiss 1 .pdf</a>
+                    </div>
+                    <p class="rowPartTwo">
+                        <a href="">
+                            <img src="img/preview.png" onMouseOver="this.src = 'img/preview-hover.png'" onmouseout="this.src = 'img/preview.png'">
+                        </a>
+                        <a href="">
+                            <img src="img/download.png" onMouseOver="this.src = 'img/download-hover.png'" onmouseout="this.src = 'img/download.png'">
+                        </a>
+                    </p>
+                </div>
+                <div class="row">
+                    <div class="rowPartOne">
+                        <img src="img/reader.png">
+                        <a href="">Planskiss 1 .pdf</a>
+                    </div>
+                    <p class="rowPartTwo">
+                        <a href="">
+                            <img src="img/preview.png" onMouseOver="this.src = 'img/preview-hover.png'" onmouseout="this.src = 'img/preview.png'">
+                        </a>
+                        <a href="">
+                            <img src="img/download.png" onMouseOver="this.src = 'img/download-hover.png'" onmouseout="this.src = 'img/download.png'">
+                        </a>
+                    </p>
+                </div>
+                <div class="row">
+                    <div class="rowPartOne">
+                        <img src="img/reader.png">
+                        <a href="">Planskiss 1 .pdf</a>
+                    </div>
+                    <p class="rowPartTwo">
+                        <a href="">
+                            <img src="img/preview.png" onMouseOver="this.src = 'img/preview-hover.png'" onmouseout="this.src = 'img/preview.png'">
+                        </a>
+                        <a href="">
+                            <img src="img/download.png" onMouseOver="this.src = 'img/download-hover.png'" onmouseout="this.src = 'img/download.png'">
+                        </a>
+                    </p>
+                </div>
+            </div>
+            <div class="line">
+
+            </div>
+
+        </div>
+    </body>
+</html>
